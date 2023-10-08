7d8fbb83db99720f09679a902f47022d99322621
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-08 23:57:07 +0300

    Feat(JS) Added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter basename="/rental-car"> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-      {/* </BrowserRouter> */}
+      <BrowserRouter basename="/rental-car">
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
