62f25aeaa69c098e4f0bcd13e017afdc07016ea9
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-09 18:03:26 +0300

    Feat(JSX) Update modal styles

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -15,20 +15,7 @@ import {
 import HeartIcon from 'components/HeartIcon/HeartIcon';
 import BasicModal from 'components/Modal/Modal';
 
-export default function CarItem({
-  data: {
-    img,
-    make,
-    model,
-    year,
-    rentalPrice,
-    address,
-    rentalCompany,
-    type,
-    mileage,
-    accessories,
-  },
-}) {
+export default function CarItem({ data }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -37,26 +24,26 @@ export default function CarItem({
     <Wrapper>
       <ImageWrap>
         <HeartIcon />
-        <Image src={img} alt="Car" />
+        <Image src={data.img} alt="Car" />
       </ImageWrap>
       <TitleWrap>
         <Title>
-          {make} {''}
-          <Span>{model}</Span>, {year}
+          {data.make} {''}
+          <Span>{data.model}</Span>, {data.year}
         </Title>
-        <Text> {rentalPrice}</Text>
+        <Text> {data.rentalPrice}</Text>
       </TitleWrap>
       <List>
-        <Item>{address.split(',')[1]}</Item>
-        <Item>{address.split(',')[2]}</Item>
-        <Item>{rentalCompany}</Item>
-        <Item>{type}</Item>
-        <Item>{model}</Item>
-        <Item>{mileage}</Item>
-        <Item>{accessories[0]}</Item>
+        <Item>{data.address.split(',')[1]}</Item>
+        <Item>{data.address.split(',')[2]}</Item>
+        <Item>{data.rentalCompany}</Item>
+        <Item>{data.type}</Item>
+        <Item>{data.model}</Item>
+        <Item>{data.mileage}</Item>
+        <Item>{data.accessories[0]}</Item>
       </List>
       <Button onClick={handleOpen} text="Learn more" width="274px" />
-      {open && <BasicModal open={open} onClose={handleClose} />}
+      {open && <BasicModal open={open} onClose={handleClose} data={data} />}
     </Wrapper>
   );
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,24 @@
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-
 import Close from '../../images/close.svg';
-
-import { Wrapper, Button, Image } from './Modal.styled';
+import Button from 'components/Button/Button';
+import {
+  Wrapper,
+  CloseIcon,
+  IconX,
+  Wrap,
+  Image,
+  TextWrap,
+  Title,
+  Span,
+  List,
+  Item,
+  Description,
+  Info,
+  ConditionItem,
+  ConditionList,
+  ConditionSpan,
+} from './Modal.styled';
 
 const style = {
   position: 'absolute',
@@ -11,13 +26,13 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 541,
-  height: 752,
+  maxHeight: 830,
   bgcolor: 'background.paper',
   borderRadius: 6,
   boxShadow: 24,
 };
 
-export default function BasicModal({ open, onClose, carId }) {
+export default function BasicModal({ open, onClose, data }) {
   return (
     <div>
       <Modal
@@ -28,9 +43,72 @@ export default function BasicModal({ open, onClose, carId }) {
       >
         <Box sx={style}>
           <Wrapper>
-            <Button onClick={onClose}>
-              <Image src={Close} alt="close-cross" />
-            </Button>
+            <CloseIcon onClick={onClose}>
+              <IconX src={Close} alt="Close button" />
+            </CloseIcon>
+            <Image src={data.img} alt={data.make} width="461" height="248" />
+            <Wrap>
+              <TextWrap>
+                <Title>
+                  {data.make} <Span>{data.model}, </Span>
+                  {data.year}
+                </Title>
+              </TextWrap>
+              <List>
+                <Item>{data.address.split(',')[1]}</Item>
+                <Item>{data.address.split(',')[2]}</Item>
+                <Item>Id: </Item>
+                <Item>Year: {data.year}</Item>
+                <Item>Type: {data.type}</Item>
+              </List>
+              <List>
+                <Item>Fuel Consumption: {data.fuelConsumption}</Item>
+                <Item>Engine Size: {data.engineSize}</Item>
+              </List>
+              <Description>{data.description}</Description>
+              <Info>Accessories and functionalities:</Info>
+              <List>
+                {data.accessories.map(item => (
+                  <Item key={item}>{item}</Item>
+                ))}
+              </List>
+              <List>
+                {data.functionalities.map(item => (
+                  <Item key={item}>{item}</Item>
+                ))}
+              </List>
+              <Info>Rental Conditions:</Info>
+              <ConditionList>
+                <ConditionItem>
+                  Minimum age:{' '}
+                  <ConditionSpan>
+                    {new Date().getFullYear() - data.year}
+                  </ConditionSpan>
+                </ConditionItem>
+                <ConditionItem>
+                  {data.rentalConditions.split('\n')[1]}
+                </ConditionItem>
+                <ConditionItem>
+                  {data.rentalConditions.split('\n')[2]}
+                </ConditionItem>
+                <ConditionItem>
+                  Mileage:{' '}
+                  <ConditionSpan>
+                    {data.mileage.toLocaleString('en-US')}
+                  </ConditionSpan>
+                </ConditionItem>
+                <ConditionItem>
+                  Price: <ConditionSpan>{data.rentalPrice}</ConditionSpan>
+                </ConditionItem>
+              </ConditionList>
+            </Wrap>
+            <Button
+              text="Rental car"
+              width="168px"
+              onClick={() => {
+                window.location.href = 'tel:+380730000000';
+              }}
+            />
           </Wrapper>
         </Box>
       </Modal>
