98300e89f7afcfa2492df84b0de9b4fbd6c8d9ed
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-10 23:15:57 +0300

    Feat(JSX) Added filters

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -12,26 +12,66 @@ import {
   UnitRight,
 } from './Filter.styled';
 
-import advertsCars from '../db/advertsCars.json';
-import Button from '../Button/Button';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import Button from 'components/Button/Button';
 
-export default function Filter() {
-  const makeOptions = [
-    ...new Set(advertsCars.map(car => ({ value: car.make, label: car.make }))),
-  ];
+export default function Filter({ makes, prices, onFilterChange }) {
+  const [selectedMake, setSelectedMake] = useState('');
+  const [selectedPriceStep, setSelectedPriceStep] = useState(null);
+  const [selectedPriceLabel, setSelectedPriceLabel] = useState('');
+  const [minValue, setMinValue] = useState('');
+  const [maxValue, setMaxValue] = useState('');
 
-  const prices = [
-    '$10',
-    '$20',
-    '$30',
-    '$40',
-    '$50',
-    '$60',
-    '$70',
-    '$80',
-    '$90',
-    '$100',
-  ];
+  const makeOptions = makes.map(make => ({ value: make, label: make }));
+
+  const priceRangeOptions = [];
+  for (let i = 30; i <= 500; i += 10) {
+    priceRangeOptions.push({ value: i, label: `${i}` });
+  }
+
+  const handlePriceStepChange = selectedOption => {
+    setSelectedPriceStep(selectedOption.value);
+    setSelectedPriceLabel(selectedOption.label);
+  };
+
+  const filteredPrices = prices.filter(price => price <= selectedPriceStep);
+
+  const formatMileage = value => {
+    const cleanedValue = value.toString().replace(/,/g, '');
+    const formattedValue = cleanedValue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return formattedValue;
+  };
+
+  const handleMinInputChange = e => {
+    setMinValue(e.target.value);
+  };
+
+  const handleMaxInputChange = e => {
+    setMaxValue(e.target.value);
+  };
+
+  const handleFilterClick = () => {
+    if (
+      parseInt(minValue.replace(/,/g, ''), 10) >
+      parseInt(maxValue.replace(/,/g, ''), 10)
+    ) {
+      toast.error('Minimum mileage cannot be greater than maximum mileage');
+      return;
+    }
+
+    const newFilters = {
+      make: selectedMake,
+      filteredPrices: filteredPrices.map(price => ({
+        value: price,
+        label: `${price}`,
+      })),
+      minMileage: parseInt(minValue.replace(/,/g, ''), 10),
+      maxMileage: parseInt(maxValue.replace(/,/g, ''), 10),
+    };
+
+    onFilterChange(newFilters);
+  };
 
   return (
     <Container>
@@ -40,6 +80,8 @@ export default function Filter() {
         <Select
           id="nameSelect"
           placeholder="Enter the text"
+          value={selectedMake}
+          onChange={selectedOption => setSelectedMake(selectedOption)}
           options={makeOptions}
           styles={{
             control: styles => ({
@@ -93,7 +135,13 @@ export default function Filter() {
         <Select
           id="priceSelect"
           placeholder="To $"
-          options={prices.map(price => ({ value: price, label: price }))}
+          value={
+            selectedPriceStep
+              ? { value: selectedPriceStep, label: selectedPriceLabel }
+              : null
+          }
+          onChange={handlePriceStepChange}
+          options={priceRangeOptions}
           styles={{
             control: styles => ({
               ...styles,
@@ -144,13 +192,21 @@ export default function Filter() {
       <Form>
         <Label>Car mileage / km</Label>
         <InputContainer>
-          <InputLeft type="text" />
+          <InputLeft
+            type="text"
+            value={formatMileage(minValue)}
+            onChange={handleMinInputChange}
+          />
           <UnitLeft>From</UnitLeft>
-          <InputRight type="text" />
+          <InputRight
+            type="text"
+            value={formatMileage(maxValue)}
+            onChange={handleMaxInputChange}
+          />
           <UnitRight>To</UnitRight>
         </InputContainer>
       </Form>
-      <Button text="Search" onClick={() => console.log()} width="135px" />
+      <Button text="Search" onClick={handleFilterClick} width="135px" />
     </Container>
   );
 }
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -3,14 +3,26 @@ import CarItem from 'components/CarItem/CarItem';
 import { WrapperFilter, WrapperList, LoadMore } from './Catalog.styled';
 import Filter from 'components/Filter/Filter';
 
-import { useGetCarsByPageQuery } from '../../redux/operations';
+import {
+  useGetAdvertsQuery,
+  useGetCarsByPageQuery,
+} from '../../redux/operations';
 import { Loader } from 'components/Loader/Loader';
 
 export default function Catalog() {
   const [page, setPage] = useState(1);
   const [allCars, setAllCars] = useState([]);
-
   const { data, error, isLoading, isFetching } = useGetCarsByPageQuery(page);
+  const { data: allAdverts } = useGetAdvertsQuery();
+
+  const [filters, setFilters] = useState({
+    make: '',
+    filteredPrices: [],
+    minMileage: '',
+    maxMileage: '',
+  });
+  const [filteredAdverts, setFilteredAdverts] = useState(null);
+  const [isFiltering, setIsFiltering] = useState(false);
 
   const loadMore = () => {
     setPage(page + 1);
@@ -18,33 +30,99 @@ export default function Catalog() {
 
   useEffect(() => {
     if (data) {
-      setAllCars(prevCars => [...prevCars, ...data]);
+      setAllCars(prevCatalog => [...prevCatalog, ...data]);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (isFiltering) {
+      if (
+        filters.make ||
+        filters.filteredPrices.length > 0 ||
+        filters.minMileage ||
+        filters.maxMileage
+      ) {
+        const filteredAdverts = allAdverts.filter(advert => {
+          if (filters.make && advert.make !== filters.make.value) {
+            return false;
+          }
+          if (
+            filters.filteredPrices.length > 0 &&
+            !filters.filteredPrices.some(
+              priceObj => priceObj.value === advert.rentalPrice.replace('$', '')
+            )
+          ) {
+            return false;
+          }
+          if (filters.minMileage && advert.mileage < filters.minMileage) {
+            return false;
+          }
+          if (filters.maxMileage && advert.mileage > filters.maxMileage) {
+            return false;
+          }
+          return true;
+        });
+
+        setFilteredAdverts(filteredAdverts);
+      } else {
+        setFilteredAdverts([]);
+      }
+    }
+  }, [filters, allAdverts, isFiltering]);
+
+  const makes = allAdverts
+    ? [...new Set(allAdverts.map(advert => advert.make))]
+    : [];
+  const prices = allAdverts
+    ? [
+        ...new Set(
+          allAdverts.map(advert => advert.rentalPrice.replace('$', ''))
+        ),
+      ]
+    : [];
+  const mileage = allAdverts
+    ? [...new Set(allAdverts.map(advert => advert.mileage))]
+    : [];
+  const minMileage = Math.min(...mileage);
+  const maxMileage = Math.max(...mileage);
 
   return (
     <>
       <WrapperFilter>
-        <Filter />
+        <Filter
+          makes={makes}
+          prices={prices}
+          minMileage={minMileage}
+          maxMileage={maxMileage}
+          onFilterChange={newFilters => {
+            setFilters(newFilters);
+            setIsFiltering(true);
+          }}
+          filters={filters}
+        />
       </WrapperFilter>
       <WrapperList>
-        {error ? (
+        {isFiltering ? (
+          filteredAdverts !== null && filteredAdverts.length > 0 ? (
+            filteredAdverts.map((car, index) => (
+              <CarItem key={index} data={car} />
+            ))
+          ) : (
+            <div>No results found for the selected criteria.</div>
+          )
+        ) : error ? (
           <>Oops, there was an error...</>
         ) : isLoading ? (
           <Loader />
         ) : allCars.length > 0 ? (
-          <>
-            {allCars.map(car => (
-              <CarItem key={car.id} data={car} />
-            ))}
-          </>
+          allCars.map((car, index) => <CarItem key={index} data={car} />)
         ) : null}
+        {!isFiltering && data && data.length >= 8 && (
+          <LoadMore variant="text" onClick={loadMore} disabled={isFetching}>
+            Load more
+          </LoadMore>
+        )}
       </WrapperList>
-      {data && data.length >= 8 && (
-        <LoadMore onClick={loadMore} disabled={isFetching}>
-          Load more
-        </LoadMore>
-      )}
     </>
   );
 }
