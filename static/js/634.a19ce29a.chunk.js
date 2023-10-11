d4dad489d7796ae76cc7b5d915d5fb0d6e6d4bff
ViktorSvertoka <victor.svertoka@gmail.com>
2023-10-11 17:21:18 +0000

    Deploying to gh-pages from @ ViktorSvertoka/rental-car@2526666cd5f49acf2a9df3a4fde2d4e391cd58c4 🚀

---
+++ static/js/634.a19ce29a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrental_car=self.webpackChunkrental_car||[]).push([[634],{7634:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});var a,i=r(168),l=r(6088).Z.div(a||(a=(0,i.Z)(["\n  padding: 60px 128px;\n"]))),t=r(3329);function c(n){var e=n.favorites;return(0,t.jsxs)(l,{children:[(0,t.jsx)("h2",{children:"Hello Favorites"}),(0,t.jsx)("ul",{children:e.map((function(n,e){return(0,t.jsxs)("li",{children:[n.make," ",n.model]},e)}))})]})}}}]);
+//# sourceMappingURL=634.a19ce29a.chunk.js.map
